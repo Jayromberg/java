ce77446dc78f7b4001ca610a7a41bb671b26e44e
Jayromberg <jayro.dev@gmail.com>
2022-08-15 09:56:14 -0300

    feat: Iniciando os estudos sobre variaveis no java

---
+++ sintaxe-variaveis-e-fluxo/src/TestOfVariables.java
@@ -0,0 +1,14 @@
+
+public class TestOfVariables {
+
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		
+		int age;
+		age = 37;
+		
+		System.out.println(age);
+		
+		System.out.println("a idade é "+ age + ", parabéns!");
+	}
+}
