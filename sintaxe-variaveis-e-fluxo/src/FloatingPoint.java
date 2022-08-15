977b5572e9bfcaf76ab095ff0c493cc9dff2a0c3
Jayromberg <jayro.dev@gmail.com>
2022-08-15 11:06:47 -0300

    feat: Sobre ponto flutuante

---
+++ sintaxe-variaveis-e-fluxo/src/FloatingPoint.java
@@ -0,0 +1,24 @@
+
+public class FloatingPoint {
+
+	public static void main(String[] args) {
+		double wage = 1275.50;
+        System.out.println(wage);
+        
+        System.out.println("meu salario é " + wage);
+        
+        //int valor = 12.5;             //não compila.
+
+        //int valor2 = 0.0;             //não compila.
+
+        //double teste = 125.50;        // compila
+
+        //int valor3 = teste;           //não compila.
+        
+        int divisao1 = 5 / 2;
+        System.out.println(divisao1);
+        
+        double divisao2 = 5.0 / 2;
+        System.out.println(divisao2);
+	}
+}
