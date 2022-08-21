565ee0ff19e85f9e0c53696eebb84f2b47d6d036
Jayromberg <jayro.dev@gmail.com>
2022-08-21 16:40:02 -0300

    feat: Exercícios sobre strings

---
+++ sintaxe-variaveis-e-fluxo/src/TestOfStrings.java
@@ -0,0 +1,17 @@
+
+public class TestOfStrings {
+
+	public static void main(String[] args) {
+		char letra = 'a';
+        System.out.println(letra);
+        
+        char valor = 66;
+        System.out.println(valor);
+        
+        valor = (char) (valor + 1);
+        System.out.println(valor);
+        
+        String palavra = "curso de Java";
+        System.out.println(palavra);
+	}
+}
